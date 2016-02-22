1cb4b2e379ed2091278b4d60d70607d7ce6be363
Lasse Fister <commander@graphicore.de>
2016-02-23 00:59:46 +0100

    Add index.html and bootstrapping code.

---
+++ app/lib/bootstrap.js
@@ -0,0 +1,16 @@
+require([
+    'lib/bower_components/Atem-RequireJS-Config/browserConfig'
+], function(
+    configure
+) {
+    "use strict";
+    var setup = {
+        baseUrl: 'lib'
+      , bowerPrefix: 'bower_components'
+      , paths: {
+            'BEF': './'
+        }
+    }
+    configure(setup, require);
+    require(['main']);
+});
