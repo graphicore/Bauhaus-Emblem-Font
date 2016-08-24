5e0c5d074e212de6b21350ecaeaa370aa699ab3d
Lasse Fister <commander@graphicore.de>
2016-08-24 17:56:04 +0200

    Much improved reflow performance.

--- app/lib/foresting/SceneBuilder.js
+++ app/lib/foresting/SceneBuilder.js
@@ -27,15 +27,12 @@ define([
         return glyph;
     };
 
-    _p._set = function(line, glyph) {
-        var lineLength
-            // To return +Infinity would be a good start for a never ending
-            // spiral line. Just to keep the thought around somewhere.
-          , maxLineLength = line.getComputedStyle().get('maxLength')
-          ;
-        line.add(glyph);
+    _p._set = function(line, glyph, styleDict, maxLineLength) {
+        var lineLength;
 
-        lineLength = line.getComputedStyle().get('length', 0);
+        line.add(glyph);
+        lineLength = styleDict.get('length', 0);
+
         // TODO: add kerning?
         if(lineLength > maxLineLength) {
             line.remove(glyph);
@@ -47,10 +44,15 @@ define([
     };
 
     _p._setLine = function(line, glyphs, index) {
-        var consumed = 0, i, l, lineIsFull;
+        var consumed = 0, i, l, lineIsFull
+            , styleDict = line.getComputedStyle()
+             // To return +Infinity would be a good start for a never ending
+             // spiral line. Just to keep the thought around somewhere.
+            , maxLength = styleDict.get('maxLength')
+            ;
 
         for(i=index,l=glyphs.length;i<l; i++) {
-            lineIsFull = this._set(line, glyphs[i]);
+            lineIsFull = this._set(line, glyphs[i], styleDict, maxLength);
             if(lineIsFull)
                 break;
             consumed += 1;
@@ -104,10 +106,10 @@ define([
     };
 
     _p._applyPatch = function(script, types) {
-        var i, l, cmd, oldType, line
+        var i, l, cmd, line
           , currentLine = null
           , offset = 0
-          , firstLineIndex, idx, lineItemIndex, type
+          , firstLineIndex = null, idx, lineItemIndex, type
             // The indexes in types correspond to the indexes in the commands
             // of the patch script. We use them to find the right line to
             // operate on and an offset from glyph indexes to line indexes.
@@ -127,14 +129,14 @@ define([
                 // indexes to the line indexes
                 offset = data[1] - lineItemIndex;
                 currentLine = line;
-                if(firstLineIndex === undefined)
+                if(firstLineIndex === null)
                     // all lines from this on will have to reflow
                     firstLineIndex = line.index;
             }
 
             idx = cmd[1] + offset;
             if(cmd[0]==='D') {
-                var del = line.splice(idx, 1);
+                line.splice(idx, 1);
                 offset -= 1;
             }
             else if (cmd[0] === 'I') {
@@ -149,6 +151,13 @@ define([
         return firstLineIndex;
     };
 
+    /**
+    This is very expensive.
+
+    I leave it in here as a reference for the current reflow method,
+    which is much faster, but also much more complex and it reuses less
+    common code than this implementation via `this._setLines(glyphs, lines);`
+
     _p.reflow = function(firstLineIndex) {
         var lines = this._scene.children.slice(firstLineIndex || 0)
           , i, l, line
@@ -161,6 +170,122 @@ define([
         }
 
         this._setLines(glyphs, lines);
+
+        // cleaning up: remove unused lines from the end of this._scene
+        lines = this._scene.children;
+        for(i=lines.length-1;i>=0;i--) {
+            line = lines[i];
+            if(line.childrenLength)
+                // no more unused lines
+                break;
+            this._scene.remove(line);
+        }
+    };
+    */
+
+    function manageLineBreaks (line, lines) {
+        var found = null
+          , i, l, newLine, children
+          ;
+        // don't check the last glyph, that's already covered
+        // below we are only interested in "new" breaks
+        // if a break would be at the end of the line, it would be
+        // alright. Hence: l=line.childrenLength-1
+        for(i=0,l=line.childrenLength-1;i<l;i++) {
+            if(line.getChild(i).baseNode.id === 'br') {
+                found = i;
+                break;
+            }
+        }
+        if(found === null)
+            return;
+        children = [];
+        for(i=found+1,l=line.childrenLength;i<l;i++)
+            children.push(line.getChild(i));
+        newLine = new Line();
+        newLine.splice(0,0, children);
+        lines.unshift(newLine);
+        if(line.parent)
+            line.parent.splice(line.index + 1, 0, newLine);
+        return true;
+    }
+
+    function getNextLine(scene, nextLines) {
+        var line;
+        if(!nextLines.length) {
+            line = new Line();
+            scene.add(line);
+            nextLines.push(line);
+        }
+        return nextLines[0];
+    }
+
+    function getNextGlyph(lines) {
+        var i, l;
+        for(i=0,l=lines.length;i<l;i++)
+            if(lines[i].childrenLength)
+                return lines[i].getChild(0);
+        return null;
+    }
+
+    /**
+     * To obey the DRY principle it would be nice to consolidate the
+     * this._setLines(glyphs) path and what this method does into a
+     * set of common subroutines.
+     *
+     * Above, the old implementation did this by using `this._setLines`
+     * However, it removed all glyphs from their lines and the reset them.
+     * That turned out to be very expensive.
+     *
+     * This method tries to change only as much of the object model tree
+     * as needed and is much faster.
+     * But, since the execution paths are very different, we may end up
+     * having different results between a fresh setting of all the lines
+     * and a reflow. That would be bad.
+     *
+     * FIXME/TODO: the Apeirophobia Error of _setLines, it would be good
+     * to detect that condition in here, too.
+     */
+    _p.reflow = function(firstLineIndex) {
+        var lines = this._scene.children.slice(firstLineIndex || 0)
+          , i, line
+          , styleDict, maxLength
+          , lastGlyph, nextLine = null, lineIsFull
+          ;
+
+        while( (line = lines.shift()) ) {
+            manageLineBreaks(line, lines);
+
+            styleDict = line.getComputedStyle();
+            maxLength = styleDict.get('maxLength');
+            if(styleDict.get('length', 0) > maxLength) {
+                // remove last glyph and unshift it to the next line
+                nextLine = null;
+                do {
+                    // What if line has no children? Is that an Apeirophobia error?
+                    // only if there are glyphs left to be set.
+                    if(!line.childrenLength) break;
+                    lastGlyph = line.splice(line.childrenLength-1, 1)[0];
+                    if(!nextLine)
+                        nextLine = getNextLine(this._scene, lines);
+                    nextLine.splice(0, 0, [lastGlyph]);
+                } while(styleDict.get('length', 0) > maxLength);
+            }
+            else { // styleDict.get('length', 0) <= maxLength)
+                // get next glyph and append it to this line
+                lastGlyph = line.childrenLength && line.getChild(line.childrenLength-1);
+                lineIsFull = lastGlyph && lastGlyph.baseNode.id === 'br';
+
+                while( !lineIsFull && (lastGlyph = getNextGlyph(lines)) ) {
+                    lineIsFull = this._set(line, lastGlyph, styleDict, maxLength);
+                    lineIsFull = lineIsFull || lastGlyph.baseNode.id === 'br';
+                }
+                if(lastGlyph && !lastGlyph.parent) {
+                    nextLine = getNextLine(this._scene, lines);
+                    nextLine.splice(0, 0, [lastGlyph]);
+                }
+            }
+        }
 
         // cleaning up: remove unused lines from the end of this._scene
         lines = this._scene.children;
@@ -222,7 +347,7 @@ define([
         newGlyphs = this.shape(text);
         patch = diff.getPatchScript(glyphs, newGlyphs);
         firstLineIndex = this._applyPatch(patch, types);
-        if(firstLineIndex !== undefined)
+        if(firstLineIndex !== null)
             this.reflow(firstLineIndex);
     };
 
