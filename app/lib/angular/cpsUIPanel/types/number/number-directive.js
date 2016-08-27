81b0347415c570ebda1025f7fc8fd9bc62925d75
Lasse Fister <commander@graphicore.de>
2016-08-27 02:49:00 +0200

    cps-ui argument parsing, work in progress, dysfunctional

--- app/lib/angular/cpsUIPanel/types/number/number-directive.js
+++ app/lib/angular/cpsUIPanel/types/number/number-directive.js
@@ -17,6 +17,12 @@ define([
 ) {
     "use strict";
 
+    // FIXME: it would be advisable to separate the view logic from the
+    // business logic. Though that's not so straaight forward, look at init
+    // where `state` is created ... all concerns (event handling/business)
+    // get's mixed. Clean separation will however be key to make this
+    // reusable code.
+
     var svgns = 'http://www.w3.org/2000/svg';
 
     function attachCircle(element, vector, r) {
@@ -220,6 +226,164 @@ define([
         doc.addEventListener('mouseup', state.stop);
     }
 
+    var UIArguments = (function() {
+    // bad indentation because this will become its own module
+
+    function UIArguments(setup, args) {
+
+        this._parseArgs(args);
+    }
+    var _p = UIArguments.prototype;
+
+    UIArguments.factory = function parseArgs(argsSetup, args) {
+        return new UIArguments(setup, args);
+    }
+
+    _p._parseKeyword = function(keywordString, i, args) {
+        var setup, data, keyword, flags
+          , consumed = 0
+          ;
+                // split by any whitespace
+        data = keywordString.split(/\s+/)
+                // remove empty entries (from beginning and end)
+                .filter(function(s){ return !!s; })
+                ;
+
+        keyword = data[0];
+        if(!(keyword in this._keywords))
+            throw new CPSUIError(
+                keyword
+                    ? 'Unknown keyword string "'+keyword+'"'
+                    : 'Empty keyword string "'+keyword+'"'
+            );
+
+        flags = new Set(data.slice(1));
+        setup = this._keywords[keyword];
+
+        setup.amount || 1
+        --> default is one, otherwise it should be bigger than 1 up to Infinity
+        --> each keyword with more than one allowed entries will be stored in an
+            array. A single appearance keyword is stored directly as value?
+        --> should we do a min max thing here?
+        --> is amount here min = max = amount?
+        --> if yes, we should also have [3,5] for min 3 max 5
+
+        setup.defaultFlags || new Set()
+        --> there are some flags that contradict each other, like min and max
+        --> so the default is min, but if there's a max, min should not be set
+        --> I'd also prefer to fail if there's more than on min or max flag
+
+
+        setup.value || or null
+        --> a function that accepts or refuses an input as value
+        --> if accepted, consumed is increased by one
+        --> could run until it refuses an input and should know the number
+            of the iteration, so it can shut down when it has enough
+
+        if(!(keyword in this._parsed))
+            this._parsed[keyword] = [];
+        this._parsed[keyword].push(result);
+        return consumed;
+    }
+
+    _p._parseArgs = function (args) {
+        var i, l, item;
+        for(i=0,l=args.length;i<l;i++) {
+            item = args[i];
+            if(typeof item !== 'string')
+                throw new CPSUIError('Expected a keyword string but got a ('
+                                        + (typeof item) + '): ' + item);
+            i += this._parseKeyword(item, i, args);
+        }
+    }
+
+    // bad indentation end;
+    })();
+
+    /**
+     *
+     * A keyword with its flags is given as one compound string
+     * if a value is following a keyword it's given as a value.
+     *
+     * The parser can and will raise errors, these can be displayed
+     * in the user interface directly.
+     *
+     * TODO: different interfaces for the same value may need differnt
+     * arguments. It would be nice to have a solution where one UI element
+     * can be configured for different interface types.
+     * We'll need a good case for this before implementing anything.
+     *
+     * // n of these
+     * "mark <flags>" value
+     *     arguments:
+     *         number (required)
+     *     flags:
+     *         "limiting"
+     *             Set the mark to be a boundary. The lower bound
+     *             will be min and the upper bound will be max.
+     *             If more than two boundaries are defined, the
+     *             boundaries with values between lower and upper
+     *             are discarded.
+     *         "min" or "max" (optional) default: "min"
+     *             min and max are only used if there is just one
+     *             boundary defined. otherwise
+     *
+     * "unbounded"
+     *     While editing, the drag handle can leave the ui-elements
+     *     canvas to give better/different feedback.
+     *     After editing the disllay will be adjusted.
+     *     (experimental)
+     *     // will need state information from the changing action
+     *     // which is kind of sub-optimal! maybe the changing
+     *     // action would have to draw itself, which is kind of
+     *     // against the current concept of using the feedback to
+     *     // update the display.
+     *
+     * "delta <flags>" magnitude
+     *     arguments:
+     *         magnitude:integer default 0 (optional)
+     *             The final delta will be calculated by:
+     *             `delta * Math.pow( 10, magnitude)`
+     *             So, a negative magnitude moves the decimal point
+     *             to the left while a positive value moves it to
+     *             the right.
+     *             Math.pow( 10, 3)
+     *                 > 1000
+     *             Math.pow( 10, 0)
+     *                 > 1
+     *             Math.pow( 10, -3)
+     *                 > 0.001
+     *             `magnitude` must be a whole number (integer).
+     *     flags:
+     *         "move" default (if feasible: test this!)
+     *         "interval"
+     * "roc <flags>"
+     *     Rate of change function.
+     *     flags:
+     *         "linear" default
+     *             The change corresponds to the raw determined
+     *             change; e.g. actual pixels of mouse movement.
+     *         "cubic"
+     *             Math.pow(change, 3), setting `delta magnitude`
+     *             to something negative may help here to make
+     *             the effect less extreme.
+     *             Using cubic here because it works nicely with
+     *             negative inputs as well.
+     *             The idea here is that the having the mouse
+     *             further away from the origin creates at some point
+     *             a bigger change than the linear version. With
+     *             lower inputs it should be more sensitive than
+     *             the linear one.
+     *         tbc. if requested.
+     * "precision" magnitude
+     *     Round to precision. maginute works as in delta and
+     *     describes the first significant decimal position, the
+     *     default (without precision) is doing nothing. To round
+     *     to integers magnitude must be 0.
+     *     arguments:
+     *         magnitude:integer (required)
+     */
+    var parseArgs = UIArguments.factory.bind(null, uiArgsumentsSetup);
 
     function numberDirective() {
         function link(scope, element, attrs, ctrl) {
@@ -235,8 +399,11 @@ define([
             element.on('mousedown', init.bind(null, ctrl.item));
 
             function draw(svg, item) {
+                // clean the slate
                 while(svg.lastChild)
                     svg.removeChild(svg.lastChild);
+
+                // parse the arguments of uiItem and draw the wiget accordingly.
 
                 var handle = attachCircle(svg, [10, 10], 5);
                 handle.setAttribute('data-is-ui', '');
