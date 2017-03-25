9436c94ddbfb3b26e2e1b3de9034fe83b1223694
Lasse Fister <commander@graphicore.de>
2017-03-25 18:15:15 +0100

    Add bin/serve for a Atem-IO RESTful filesystem.

---
+++ bin/node-config.js
@@ -0,0 +1,16 @@
+/* globals process */
+define(['../app/lib/bower_components/Atem-RequireJS-Config/nodeConfig'],
+function(configure) {
+    var path = require('path')
+      , rootDir = path.dirname(path.dirname(process.mainModule.filename))
+      , setup = {
+            baseUrl: rootDir + '/app/lib'
+          , bowerPrefix: 'bower_components'
+          , paths: {
+                'BEF': './'
+            }
+        }
+      ;
+    define('rootDir', rootDir);
+    return configure.bind(null, setup);
+});
